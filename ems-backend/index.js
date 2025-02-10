22452557605aeba20e27a9f2cd627677c236c915
Karan Thakkar <kmhthakkar@gmail.com>
2025-02-11 00:06:32 +0530

    change cors settings

--- ems-backend/index.js
+++ ems-backend/index.js
@@ -13,7 +13,8 @@ connectDB();
 const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json({
@@ -36,6 +37,6 @@ app.get('/', (req, res) => {
 app.use('/api/auth', AuthController);
 app.use('/api/event', EventController);
 
-app.listen(port, () => {    
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
